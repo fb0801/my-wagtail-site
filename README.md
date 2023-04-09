95e58ddf57cc0d88ad14bfe69bdf0db67a550765
fb0801 <fbhatti0801@gmail.com>
2023-04-09 18:24:21 +0100

    lets get started

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # my-wagtail-site
+
+This project looks at how the Wagtail CMS works as we use it to create a blog 
+
+<h3>Resources used: </h3>
+https://docs.wagtail.org/en/stable/getting_started/tutorial.html
+
+https://www.youtube.com/watch?v=mbUFWkZAm8w
+
+https://www.toptal.com/developers/gitignore/
---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ld9=)(=qk-wub!d!!(+ztc9knvy0x(awubhqc_y-r92yd^913i"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
